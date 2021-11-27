60658ce6712a8c0f7e5f3f49a1792696c9f124c2
qbosterj <tangqiangan608@yeah.net>
2021-11-27 21:21:28 +0800

    增加监测回调，版本号提升为2.2.0

--- MobileTracking/libmobiletracking/src/main/java/cn/com/mma/mobile/tracking/api/Constant.java
+++ MobileTracking/libmobiletracking/src/main/java/cn/com/mma/mobile/tracking/api/Constant.java
@@ -16,10 +16,8 @@ public class Constant {
 	 * 对应<offlineCache>中的<timeout>字段
 	 */
 	public static int OFFLINECACHE_TIMEOUT = 15;
-
     /* 正常队列间隔周期 10s */
     public static int ONLINECACHE_QUEUEEXPIRATIONSECS = 10;
-
 	/**
 	 * 先获得位置信息，如需要才发送给服务器，这样能减少延迟。
 	 */
